9bee105df88ec78b2a731d54e51358507a652901
Bohan Liu <bohan3@illinois.edu>
2021-10-04 16:52:08 -0500

    1.2.0

--- injector.js
+++ injector.js
@@ -5,5 +5,4 @@ function injectScript(file, node) {
     s.setAttribute('src', file);
     th.appendChild(s);
 }
-injectScript( chrome.extension.getURL('content-script.js'), 'body');
-
+setTimeout( injectScript( chrome.extension.getURL('content-script.js'), 'body'), 1000);
